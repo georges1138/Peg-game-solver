18e28b351dc8721f301fbb363a32d047ff1b8f42
georges1138 <geods73@yahoo.com>
2024-10-17 11:31:16 -0400

    This was an overhaul to the code to organize the functions better and improve performant.

--- main.py
+++ main.py
@@ -1,155 +1,154 @@
-# This is a sample Python script.  It uses recursion to solve the Peg Board Game.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from dataclasses import dataclass
+from typing import ClassVar
 import time
 
+"""
+This Python program solves the Peg Board game.  The user will initialing pick the first empty hole on the board,
+then the program will transverse the valid moves until only one peg is left.  It uses the Depth-First search algorithm
+to track each move and to reverse if an unsuccessful finish is reached.
+"""
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-# 'Finished' flag
-done = False
-
-# List to track the peg moves
-moves = []
-
-# The Peg Board
-markers = ['X','X','X','X','X','X','X','X','X','X','X','X','X','X','X']
-##########  1   2   3   4   5   6   7   8  9   10  11  12  13   14  15
-##markers = ['O','O','O','X','X','O','X','X','O','O','O','X','X','O','O']
-
-# The rules of possible moves for each position on the board
-rules = [[(4,2),(6,3)],[(7,4),(9,5)],[(8,5),(10,6)],[(1,2),(6,5),(11,7),(13,8)],[(12,8),(14,9)],[(1,3),(4,5),(13,9),(15,10)],
-    [(2,4),(9,8)],[(3,5),(10,9)],[(2,5),(7,8)],[(3,6),(8,9)],
-    [(4,7),(13,12)],[(5,8),(14,13)],[(4,8),(6,9),(11,12),(15,14)],
-    [(5,9),(12,13)],[(6,10),(13,14)]]
-
+@dataclass(slots=True)
 class Peg:
-
-    def __init__(self, number, moves):
-        self.number = number
-        self.moves = moves
-
-    def __str__(self):
-        return '{self.number} with moves {self.moves}'.format(self=self)
+    number: int
+    moves: list[tuple, ...]
 
 
 class Board:
+    # Class variable that holds the move rules for each position on the peg board.
+    rules: ClassVar[list[list[tuple, ...]]] = [
+        [(4, 2), (6, 3)],
+        [(7, 4), (9, 5)],
+        [(8, 5), (10, 6)],
+        [(1, 2), (6, 5), (11, 7), (13, 8)],
+        [(12, 8), (14, 9)],
+        [(1, 3), (4, 5), (13, 9), (15, 10)],
+        [(2, 4), (9, 8)],
+        [(3, 5), (10, 9)],
+        [(2, 5), (7, 8)],
+        [(3, 6), (8, 9)],
+        [(4, 7), (13, 12)],
+        [(5, 8), (14, 13)],
+        [(4, 8), (6, 9), (11, 12), (15, 14)],
+        [(5, 9), (12, 13)],
+        [(6, 10), (13, 14)]
+    ]
 
     def __init__(self):
+        self.markers = ['X','X','X','X','X','X','X','X','X','X','X','X','X','X','X']
         self.all_pegs = []
 
-        for i in range(len(rules)):
-            created_peg = Peg(i + 1, rules[i])
-            self.all_pegs.append(created_peg)
+        for i, r in enumerate(Board.rules, start=1):
+            self.all_pegs.append(Peg(i, r))
 
-def first_peg():
+
+def first_peg_position() -> int:
+    """
+    Function that will take a user's input as the first empty peg hole.
+    :return:
+    """
     while True:
         try:
-            firsthole = int(input("Please enter the starting hole: "))
-        except:
-            print("Invalid entry.  Please enter a number.")
+            first_hole = int(input("Please enter the starting hole: "))
+        except ValueError:
+            print("Invalid entry.  Please enter a number: 1, 4, 5, 6, or 13:")
             continue
-        else:
-            if firsthole not in [1, 4, 5, 6, 13]:
-                print("Invalid starting hole.  Please enter again.")
-                continue
-            else:
-                print(f'Confirmed: Starting at position {firsthole}')
-                return firsthole
+
+        if first_hole not in (1, 4, 5, 6, 13):
+            print("Invalid starting hole.  Please enter a number: 1, 4, 5, 6, or 13:")
+            continue
+        print(f"Confirmed: Starting at position: {first_hole}")
+        return first_hole
 
 
-def transverse_board(board, markers, xp):
-    global done
-    global moves
-    # pass
+def transverse_board(board, pos, moves) -> None:
+    """
+    This is the recursive function that will do a depth-first search approach to solve the
+    Peg board game.
+
+    :param board:
+    :param pos:
+    :param moves:
+    :return:
+    """
+    # check base case
+    if check_finish(board):
+        return
+
     # get the moves from current position
-    for i in range(len(markers)):
-        if markers[i] == 'O':
-            curpeg = board.all_pegs[i]
-            # print(curpeg)
-            # For every valid move...
-            for i in range(len(curpeg.moves)):
-                x, y = curpeg.moves[i]
-                # print(f'Test move: {x},{y}')
-                # Make sure there are pegs
-                if markers[x - 1] == 'X' and markers[y - 1] == 'X':
-                    print(f"Move: {x}->{curpeg.number} - remove {y}")
-                    markers[curpeg.number - 1] = 'X'
-                    markers[x - 1] = 'O'
-                    markers[y - 1] = 'O'
-                    moves.append((x, y))
-                    # print("adding move:")
-                    # print(moves)
-                    # print(markers)
-                    done = check_finish(markers)
-                    if done:
-                        return
-                    ### Restart the cycle with new peg positions ***
-                    transverse_board(board, markers, curpeg.number)
-                    if done:
-                        return
-                elif markers[x - 1] == 'O' or markers[y - 1] == 'O':
-                    # There's a missing peg, Cannot move from here
-                    # print('Cannot move, going to the next move:')
-                    continue
-        else:
-            # print(f'{i+1}: Peg hole not empty, skipping:')
-            continue
+    for i, (m, current_peg) in enumerate(zip(board.markers, board.all_pegs)):
+        if m == 'O':
+            # If space is open, check every valid move...
+            for j in range(len(current_peg.moves)):
+                x, y = current_peg.moves[j]
 
-    # print("Got Here!!!----------------------")
-    if not done:
+                # Make sure there are pegs
+                if board.markers[x - 1] == 'X' and board.markers[y - 1] == 'X':
+                    print(f"Move: {x}->{current_peg.number} - remove {y}")
+                    board.markers[current_peg.number - 1] = 'X'
+                    board.markers[x - 1] = 'O'
+                    board.markers[y - 1] = 'O'
+                    moves.append((x, y))
+
+                    ### Restart the cycle with new peg positions ***
+                    transverse_board(board, current_peg.number, moves)
+                    if check_finish(board):
+                        return
+                elif board.markers[x - 1] == 'O' or board.markers[y - 1] == 'O':
+                    # There's a missing peg, Cannot move from here
+                    continue
+
+    if not check_finish(board):
         print("Exhausted all moves, but still more than 1 peg: failed!")
-        x, y = moves[-1]
+        x, y = moves.pop()
         print(f'Undo last move: {x} - {y}')
         # Put the pegs back from last move and move to next moves
-        markers[x - 1] = 'X'
-        markers[y - 1] = 'X'
-        markers[xp - 1] = 'O'
-        moves.pop(-1)
-        # print("after")
-        # print(moves)
+        board.markers[x - 1] = 'X'
+        board.markers[y - 1] = 'X'
+        board.markers[pos - 1] = 'O'
         return
 
 
-def check_finish(markers):
+def check_finish(board: Board) -> bool:
+    """
+    Boolean function to check if the peg board only has one peg left, therefore successfully completed.
+    :param board:
+    :return:
+    """
     count = 0
-    # check to see if only one peg remains
-    for i in range(len(markers)):
-        if markers[i] == 'X':
+    for p in board.markers:
+        if p == 'X':
             count += 1
+        if count > 1:
+            return False
 
     if count == 1:
-        print("done.")
         return True
-    else:
-        # not done
-        return False
+    return False
 
 
-print("Welcome to the Peg Board Game!")
+def main() -> None:
+    """
+    The main routine function.
+    :return:
+    """
+    board = Board()
+    moves_stack = []
 
-start_time = time.time()
-#First, Setup
-board = Board()
-pos = first_peg()
-markers[pos-1] = 'O'
-print(markers)
-print("Starting...")
-#And away we go
-transverse_board(board, markers, pos)
-print(moves)
-print("Back in main")
-print(markers)
-end_time = time.time()
-elapsed_time = end_time - start_time
+    print("Welcome to the Peg Board Game Solver!")
+    start_time = time.perf_counter()
+    peg_pos = first_peg_position()
+    board.markers[peg_pos-1] = 'O'
+    print(f"{board.markers=}")
 
-print(elapsed_time)
+    print(f"Starting...")  # And away we go!
+    transverse_board(board, peg_pos, moves_stack)
+    print(f"{moves_stack=}")
+    print(f"{board.markers=}")
+
+    end_time = time.perf_counter()
+    print(f"Time to complete: {end_time - start_time}")
+
+
+if __name__ == "__main__":
+    main()
